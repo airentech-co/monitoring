1cc5630e6b724016a94819b5a8ee29fa8e7702fe
orionstar65 <orionstar0605@gmail.com>
2025-07-27 23:31:36 -0400

    temp ubuntu

---
+++ Ubuntu/build/CMakeFiles/MonitorClient_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for MonitorClient_autogen.
+# This may be replaced when dependencies are built.
---
+++ Ubuntu/build/MonitorClient_autogen/EWIEGA46WW/moc_MonitorClient.cpp
@@ -0,0 +1,316 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'MonitorClient.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.13)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../MonitorClient.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'MonitorClient.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.13. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MonitorClient_t {
+    QByteArrayData data[32];
+    char stringdata0[436];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MonitorClient_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MonitorClient_t qt_meta_stringdata_MonitorClient = {
+    {
+QT_MOC_LITERAL(0, 0, 13), // "MonitorClient"
+QT_MOC_LITERAL(1, 14, 6), // "screen"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 3), // "tic"
+QT_MOC_LITERAL(4, 26, 14), // "browserHistory"
+QT_MOC_LITERAL(5, 41, 6), // "keyLog"
+QT_MOC_LITERAL(6, 48, 6), // "usbLog"
+QT_MOC_LITERAL(7, 55, 8), // "usbEvent"
+QT_MOC_LITERAL(8, 64, 9), // "getScreen"
+QT_MOC_LITERAL(9, 74, 7), // "sendTic"
+QT_MOC_LITERAL(10, 82, 20), // "sendBrowserHistories"
+QT_MOC_LITERAL(11, 103, 11), // "sendKeyLogs"
+QT_MOC_LITERAL(12, 115, 11), // "sendUSBLogs"
+QT_MOC_LITERAL(13, 127, 14), // "handleKeyPress"
+QT_MOC_LITERAL(14, 142, 9), // "timestamp"
+QT_MOC_LITERAL(15, 152, 11), // "windowTitle"
+QT_MOC_LITERAL(16, 164, 7), // "keyText"
+QT_MOC_LITERAL(17, 172, 14), // "handleUsbEvent"
+QT_MOC_LITERAL(18, 187, 3), // "run"
+QT_MOC_LITERAL(19, 191, 16), // "enableScreenshot"
+QT_MOC_LITERAL(20, 208, 7), // "enabled"
+QT_MOC_LITERAL(21, 216, 12), // "enableKeylog"
+QT_MOC_LITERAL(22, 229, 20), // "enableBrowserHistory"
+QT_MOC_LITERAL(23, 250, 19), // "enableUsbMonitoring"
+QT_MOC_LITERAL(24, 270, 19), // "onTrayIconActivated"
+QT_MOC_LITERAL(25, 290, 33), // "QSystemTrayIcon::ActivationRe..."
+QT_MOC_LITERAL(26, 324, 6), // "reason"
+QT_MOC_LITERAL(27, 331, 15), // "onSendAllAction"
+QT_MOC_LITERAL(28, 347, 17), // "onConfigureAction"
+QT_MOC_LITERAL(29, 365, 22), // "onTakeScreenshotAction"
+QT_MOC_LITERAL(30, 388, 22), // "onTestConnectionAction"
+QT_MOC_LITERAL(31, 411, 24) // "onTestAPIEndpointsAction"
+
+    },
+    "MonitorClient\0screen\0\0tic\0browserHistory\0"
+    "keyLog\0usbLog\0usbEvent\0getScreen\0"
+    "sendTic\0sendBrowserHistories\0sendKeyLogs\0"
+    "sendUSBLogs\0handleKeyPress\0timestamp\0"
+    "windowTitle\0keyText\0handleUsbEvent\0"
+    "run\0enableScreenshot\0enabled\0enableKeylog\0"
+    "enableBrowserHistory\0enableUsbMonitoring\0"
+    "onTrayIconActivated\0"
+    "QSystemTrayIcon::ActivationReason\0"
+    "reason\0onSendAllAction\0onConfigureAction\0"
+    "onTakeScreenshotAction\0onTestConnectionAction\0"
+    "onTestAPIEndpointsAction"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MonitorClient[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      24,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       6,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  134,    2, 0x06 /* Public */,
+       3,    0,  135,    2, 0x06 /* Public */,
+       4,    0,  136,    2, 0x06 /* Public */,
+       5,    0,  137,    2, 0x06 /* Public */,
+       6,    0,  138,    2, 0x06 /* Public */,
+       7,    0,  139,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       8,    0,  140,    2, 0x0a /* Public */,
+       9,    0,  141,    2, 0x0a /* Public */,
+      10,    0,  142,    2, 0x0a /* Public */,
+      11,    0,  143,    2, 0x0a /* Public */,
+      12,    0,  144,    2, 0x0a /* Public */,
+      13,    3,  145,    2, 0x0a /* Public */,
+      17,    0,  152,    2, 0x0a /* Public */,
+      18,    0,  153,    2, 0x0a /* Public */,
+      19,    1,  154,    2, 0x0a /* Public */,
+      21,    1,  157,    2, 0x0a /* Public */,
+      22,    1,  160,    2, 0x0a /* Public */,
+      23,    1,  163,    2, 0x0a /* Public */,
+      24,    1,  166,    2, 0x0a /* Public */,
+      27,    0,  169,    2, 0x0a /* Public */,
+      28,    0,  170,    2, 0x0a /* Public */,
+      29,    0,  171,    2, 0x0a /* Public */,
+      30,    0,  172,    2, 0x0a /* Public */,
+      31,    0,  173,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Int,
+    QMetaType::Void, QMetaType::LongLong, QMetaType::QString, QMetaType::QString,   14,   15,   16,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   20,
+    QMetaType::Void, QMetaType::Bool,   20,
+    QMetaType::Void, QMetaType::Bool,   20,
+    QMetaType::Void, QMetaType::Bool,   20,
+    QMetaType::Void, 0x80000000 | 25,   26,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MonitorClient::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MonitorClient *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->screen(); break;
+        case 1: _t->tic(); break;
+        case 2: _t->browserHistory(); break;
+        case 3: _t->keyLog(); break;
+        case 4: _t->usbLog(); break;
+        case 5: _t->usbEvent(); break;
+        case 6: { int _r = _t->getScreen();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 7: { int _r = _t->sendTic();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 8: { int _r = _t->sendBrowserHistories();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 9: { int _r = _t->sendKeyLogs();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 10: { int _r = _t->sendUSBLogs();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = std::move(_r); }  break;
+        case 11: _t->handleKeyPress((*reinterpret_cast< qint64(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2])),(*reinterpret_cast< const QString(*)>(_a[3]))); break;
+        case 12: _t->handleUsbEvent(); break;
+        case 13: _t->run(); break;
+        case 14: _t->enableScreenshot((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 15: _t->enableKeylog((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 16: _t->enableBrowserHistory((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 17: _t->enableUsbMonitoring((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 18: _t->onTrayIconActivated((*reinterpret_cast< QSystemTrayIcon::ActivationReason(*)>(_a[1]))); break;
+        case 19: _t->onSendAllAction(); break;
+        case 20: _t->onConfigureAction(); break;
+        case 21: _t->onTakeScreenshotAction(); break;
+        case 22: _t->onTestConnectionAction(); break;
+        case 23: _t->onTestAPIEndpointsAction(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MonitorClient::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MonitorClient::screen)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (MonitorClient::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MonitorClient::tic)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (MonitorClient::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MonitorClient::browserHistory)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (MonitorClient::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MonitorClient::keyLog)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (MonitorClient::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MonitorClient::usbLog)) {
+                *result = 4;
+                return;
+            }
+        }
+        {
+            using _t = void (MonitorClient::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MonitorClient::usbEvent)) {
+                *result = 5;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MonitorClient::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_MonitorClient.data,
+    qt_meta_data_MonitorClient,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MonitorClient::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MonitorClient::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MonitorClient.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int MonitorClient::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 24)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 24;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 24)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 24;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MonitorClient::screen()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void MonitorClient::tic()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void MonitorClient::browserHistory()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+
+// SIGNAL 3
+void MonitorClient::keyLog()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, nullptr);
+}
+
+// SIGNAL 4
+void MonitorClient::usbLog()
+{
+    QMetaObject::activate(this, &staticMetaObject, 4, nullptr);
+}
+
+// SIGNAL 5
+void MonitorClient::usbEvent()
+{
+    QMetaObject::activate(this, &staticMetaObject, 5, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
